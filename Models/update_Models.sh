ffc9185a31e382e994c9311ed8ed2117d47694bf
everett.165 <everett.165@unity-1.asc.ohio-state.edu>
2018-09-11 16:35:55 -0400

    newer scripts to get models and update

--- Models/update_Models.sh
+++ Models/update_Models.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-cd TRENTO
-#git stash
-git pull
-
-cd ../part2t
-#git stash
-git pull
-
-cd ../iS3D
-#git stash
-git pull
-
-cd ../gpu-vh
-#git stash
-git pull
-
-cd ../freestream-milne
-#git stash
-git pull
-
-cd ../cpu-vh
-#git stash
-git pull
-
+for dir in */; do
+    cd $dir
+    git fetch
+    git stash
+    git pull origin master
+    cd ../
+done
 
